fc82fb7cdd033a91be3ce87388a4843b6c6a1320
RiaMang <1riamanglani@gmail.com>
2015-06-06 22:37:52 -0400

    First

---
+++ Cars/Controllers/CarController.cs
@@ -0,0 +1,25 @@
+﻿using Cars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Cars.Controllers
+{   
+    [RoutePrefix("api/Car")]
+    public class CarController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Route("GetCarsFromYear")]
+        public async Task<List<Car>> GetCarsFromYear(string year)
+        {
+            return await db.GetCarsFromYear(year);
+        }
+
+
+    }
+}
---
+++ Cars/Migrations/201506041526076_NoChange1.cs
@@ -0,0 +1,16 @@
+namespace Cars.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NoChange1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
